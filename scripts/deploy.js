cef44f54d1b6ddbec86be076027f607cb652c0f9
anshu-csigma <anshur@csigma.finance>
2024-11-22 16:30:26 +0530

    removed unnessery

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
     const Escrow = await hre.ethers.getContractFactory("Escrow");
     const deployer = (await hre.ethers.getSigners())[0];
-    const managerAddress = "0x0fD877BdE88E84EAfE882d20315114ddfe2f3459"; // Replace with the actual manager address.
+    const managerAddress = "0x0fD877BdE88E84EAfE882d20315114ddfe2f3459"; 
 
 
     console.log("Deploying contract with the deployer address:", deployer.address);
